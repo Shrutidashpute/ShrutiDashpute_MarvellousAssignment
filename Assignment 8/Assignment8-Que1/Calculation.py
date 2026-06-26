58e7d5e22841de69f83d17f90d572a8077477100
Shrutidashpute <87578134+Shrutidashpute@users.noreply.github.com>
2026-06-27 02:13:55 +0530

    Add files via upload

---
+++ Assignment 8/Assignment8-Que1/Calculation.py
@@ -0,0 +1,11 @@
+def add(x, y):
+    return x + y
+
+# function to multiply two numbers
+def multiply(x, y):
+    return x * y
+
+if __name__ == "__main__":
+    print("User-Defined functions")
+    print(add(2, 3))
+    print(multiply(2, 3))
---
+++ Assignment 8/Assignment8-Que1/ImportCalculationMain.py
@@ -0,0 +1,4 @@
+import Calculation
+
+print("Addition:", Calculation.add(10, 5))
+print("Multiplication:", Calculation.multiply(4, 6))
